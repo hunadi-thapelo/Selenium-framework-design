f89ee0fcaca8ee9d3cc4129ff3e47318aa28a080
hunadim <hmapulane@gmail.com>
2023-01-02 05:14:06 +0200

    Actions class to handle auto-suggestive dropdown and validate order confirmation message

--- src/test/java/EcommerceE2Eautomation/StandAloneTest.java
+++ src/test/java/EcommerceE2Eautomation/StandAloneTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -52,12 +53,21 @@ public class StandAloneTest {
         List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
         Boolean match = cartProducts.stream().anyMatch(cartProduct-> cartProduct.getText().equalsIgnoreCase(productName));
         Assert.assertTrue(match);
-
         driver.findElement(By.cssSelector(".totalRow button")).click();
 
+        //Test Case: Select India from country list under Shipping Information
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector(".user__address input")), "ind").build().perform();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+        driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[3]")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
 
-
+        //Test case: Validate order confirmation
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".hero-primary")));
+        String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+        driver.close();
 
     }
 }
