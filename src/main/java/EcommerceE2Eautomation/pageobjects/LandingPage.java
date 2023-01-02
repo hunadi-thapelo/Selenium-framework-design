82fddcb38b2d44c72950eaa96e05567762a14484
hunadim <hmapulane@gmail.com>
2023-01-02 09:52:16 +0200

    implemented FindBy annotation type with PageFactory and create loginApplication action method

--- src/main/java/EcommerceE2Eautomation/pageobjects/LandingPage.java
+++ src/main/java/EcommerceE2Eautomation/pageobjects/LandingPage.java
@@ -1,8 +1,10 @@
 package EcommerceE2Eautomation.pageobjects;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LandingPage {
     WebDriver driver;
@@ -11,10 +13,25 @@ public class LandingPage {
     {
         //initialization code
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    WebElement userEmail = driver.findElement(By.id("userEmail"));
-
+    //WebElement userEmail = driver.findElement(By.id("userEmail"));
     //PageFactory
+    @FindBy(id="userEmail") WebElement userEmail;
+    @FindBy(id="userPassword") WebElement passWord;
+    @FindBy(id="login") WebElement submit;
+
+    public void loginApplication(String email, String myPassword) //login action method
+    {
+        userEmail.sendKeys(email);
+        passWord.sendKeys(myPassword);
+        submit.click();
+    }
+
+    public void goTo()
+    {
+        driver.get("https://rahulshettyacademy.com/client");
+    }
 
 }
