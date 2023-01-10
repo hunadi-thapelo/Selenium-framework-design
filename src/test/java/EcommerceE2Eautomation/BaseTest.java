1b950afc494d5da87e7ff4055c73806dd2c7fb46
hunadim <hmapulane@gmail.com>
2023-01-10 23:24:23 +0200

    Add object of Properties class

--- src/test/java/EcommerceE2Eautomation/BaseTest.java
+++ src/test/java/EcommerceE2Eautomation/BaseTest.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseTest {
 
     public void initializeDriver()
     {
+        Properties prop = new Properties(); //obj of Properties class
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
