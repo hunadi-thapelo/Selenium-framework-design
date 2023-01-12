5905e64d0398ce794574560bf7be555274382cca
hunadim <hmapulane@gmail.com>
2023-01-12 23:53:36 +0200

    add getProperty method to dynamically get properties path

--- src/test/java/EcommerceE2Eautomation/BaseTest.java
+++ src/test/java/EcommerceE2Eautomation/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
     public void initializeDriver() throws IOException {
         Properties prop = new Properties(); //obj of Properties class
 
-        FileInputStream fis = new FileInputStream("/Users/hunadimapulane/IdeaProjects/SeleniumFrameworkDesign/src/main/java/EcommerceE2Eautomation/resources/GlobalData.properties");
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/EcommerceE2Eautomation/resources/GlobalData.properties");
         prop.load(fis);//fileinputstream object passed to prop load method
         String browserName = prop.getProperty("browser");
 
